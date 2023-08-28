de6bea354404c18bb2b07b7dc6e4b31b0978f6f1
kmoilane <karri.moilanen@gmail.com>
2023-08-28 12:25:43 +0300

    Changed proto directory

--- group1/proto/build/run_simulation.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-tmux new-window "./Simulation"
-
-# Run the second executable in another new tmux window
-tmux split-window "./controller"
-
-# Attach to the tmux session
-tmux attach-session -d
