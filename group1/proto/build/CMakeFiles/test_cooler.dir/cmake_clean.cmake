06b6ebc313680aaf40022402e3597b8e90e10af0
holle1234 <holle1234@github.com>
2023-08-29 17:54:36 +0300

    build rm

--- group1/proto/build/CMakeFiles/test_cooler.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/test_cooler"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/test_cooler.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- group1/proto/build/CMakeFiles/test_heaters.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/test_heaters"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/test_heaters.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
